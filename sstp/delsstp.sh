248c9b80154186655370f64932c9e085b5f4c887
bolodewe87 <100638260+bolodewe87@users.noreply.github.com>
2022-03-01 05:39:13 +0700

    Update delsstp.sh

--- sstp/delsstp.sh
+++ sstp/delsstp.sh
@@ -13,18 +13,6 @@ LIGHT='\033[0;37m'
 # ==========================================
 # Getting
 MYIP=$(wget -qO- ipinfo.io/ip);
-echo "Checking VPS"
-IZIN=$( curl https://raw.githubusercontent.com/senowahyu62/perizinan/main/ipvps.txt | grep $MYIP )
-if [ $MYIP = $MYIP ]; then
-echo -e "${NC}${GREEN}Permission Accepted...${NC}"
-else
-echo -e "${NC}${RED}Permission Denied!${NC}";
-echo -e "${NC}${LIGHT}Please Contact Admin!!"
-echo -e "${NC}${LIGHT}Facebook : https://m.facebook.com/lis.tio.718"
-echo -e "${NC}${LIGHT}WhatsApp : 081545854516"
-echo -e "${NC}${LIGHT}Telegram : https://t.me/Akbar218"
-exit 0
-fi
 clear
 NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/var/lib/akbarstorevpn/data-user-sstp")
 	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
@@ -59,4 +47,4 @@ echo "=========================="
 echo "Username  : $user"
 echo "Expired   : $exp"
 echo "=========================="
-echo "Script By Akbar Maulana"
+echo "Script By zerossl"

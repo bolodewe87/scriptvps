be0aa760a3e1ef209f415720f2f4b9409f7e9afa
bolodewe87 <100638260+bolodewe87@users.noreply.github.com>
2022-03-01 09:44:14 +0700

    Update edu.sh

--- websocket/edu.sh
+++ websocket/edu.sh
@@ -90,7 +90,7 @@ User=root
 CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 NoNewPrivileges=true
-ExecStart=/usr/bin/python -O /usr/local/bin/sshws-tls 2443
+ExecStart=/usr/bin/python -O /usr/local/bin/sshws-tls 8880
 Restart=on-failure
 
 [Install]
